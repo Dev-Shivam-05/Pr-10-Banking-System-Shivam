7a69d65f211849773cbc3cdb3ca1ef026dddb03d
Dev-Shivam-05 <shivambhadoriya1605@gmail.com>
2025-05-12 11:11:25 +0530

    Second Commit

--- question-1.cpp
+++ question-1.cpp
@@ -9,35 +9,35 @@ public:
     double accountBalance;
     double deposit_amount;
     double withdraw_amount;
-    double total_withdraw_amount = 0;
 public:
-    BankAccount()
+    void setDetails()
     {
         cout << "Enter Account Number :- ";
         cin >> this->accountNumber;
         cin.ignore();
         cout << endl << "Enter Account Holder Name :- ";
         getline(cin, accountHolderName);
-        cout << endl << "Enter Account Balance :- ";
+        cout << "Enter Account Balance :- ";
         cin >> this->accountBalance;
     }
 
-    void deposit(double deposit_amount)
+    void deposit()
     {
+        cout << endl;
         cout << "Enter A Amount To Deposit :- ";
         cin >> this->deposit_amount;
         accountBalance = accountBalance + deposit_amount;
     }
-    void withdraw(double withdraw_amount)
+    void withdraw()
     {
+        cout << endl;
         cout << "Enter A Amount To Withdraw :- ";
         cin >> this->withdraw_amount;
-        total_withdraw_amount += withdraw_amount;
         accountBalance = accountBalance - withdraw_amount;
     }
-    void getBalance()
+    double getBalance()
     {
-        cout << "Current Balance :- " << accountBalance << endl;
+        return accountBalance;
     }
     void displayAccountInfo()
     {
@@ -45,56 +45,146 @@ public:
         cout << "Account Number :- " << accountNumber << endl;
         cout << "Account Holder Name :- " << accountHolderName << endl;
         cout << "Account Balance :- " << accountBalance << endl;
+        cout << endl;
     }
 };
 
 class SavingAccount : public BankAccount 
 {
-protected:
-    double intrestRate;
-
 public:
+    double intrest_rate;
+
     void calculateIntrest()
     {
-        intrestRate = (accountBalance * 0.05) / 100;
-        cout << "Your Bank Is Providing "<< intrestRate <<" Rupeess Of Intrest. " << endl;
+        cout << endl;
+        intrest_rate = (accountBalance * 0.05) / 100;
+        cout << "Your Bank Is Providing "<< intrest_rate <<" Rupees Of Intrest." << endl;
+        cout << endl;
     }
 };
 
 class CheckingAccount : public BankAccount
 {
 public:
-    double overDraftLimit = 500000;
+    double overDraftLimit = 5000;
     void DraftCheaker()
     {
-        if (total_withdraw_amount > overDraftLimit)
+        if (accountBalance < overDraftLimit)
         {
+            cout << endl;
             cout << endl << "You Have Exceeced The Draft Limit.." << endl;
+            cout << endl;
         }
         else
         {
+            cout << endl;
             cout << "You Haven't Exceeced The Draft Limit..";
+            cout << endl;
         }
     }
 };
 
-class FixedDepositAmount : public BankAccount
+class FixedDepositAmount : public BankAccount 
 {
 public:
     int term;
-    double Fixed_Deposite_Intrest;
+    float fixed_deposit_intrest;
     void calculateIntrest()
     {
-        cout << "Enter Terms (Time Duration In Months) :- ";
+        cout << "Enter Time Duration (In Month's) :- ";
         cin >> term;
-        Fixed_Deposite_Intrest = (accountBalance * 0.02);
-        cout << "Your Bank Is Providing "<< Fixed_Deposite_Intrest <<" Rupees Of Intrest.";
+        fixed_deposit_intrest = (accountBalance * 0.02);
+        cout << endl;
+        cout << "Your Bank Will Provide "<< fixed_deposit_intrest <<" Rupees Of Intrest In "<< term <<" Months." << endl;
+        cout << endl;
     }
 };
 
 int main()
 {
-    FixedDepositAmount obj3;
-    obj3.calculateIntrest();
+    BankAccount obj;
+    obj.setDetails();
+
+    SavingAccount Myobj_2;
+    CheckingAccount Myobj_3;
+    FixedDepositAmount Myobj_4;
+
+    int main_choice;
+    cout << endl << "----- Welcome To Our Bank -----" << endl;
+    do
+    {
+        cout << endl << "Press 1 For Deposit / Withdraw Or Account Details" << endl;
+        cout << "Press 2 For Saving Accounts" << endl;
+        cout << "Press 3 For Over draft Checking" << endl;
+        cout << "Press 4 For FD (Fixed Deposit)" << endl;
+        cout << "Press 0 To Exit" << endl;
+        cout << "Enter your Choice :- ";
+        cin >> main_choice;
+
+        switch (main_choice)
+        {
+            case 0:
+                cout << endl << "---- Program Is Scucessfully Exited----" << endl;
+                break;
+            case 1:
+                int choice_1;
+                do
+                {
+                    cout << endl << "Press 1 To Deposit" << endl;
+                    cout << "Press 2 To Withdraw" << endl;
+                    cout << "Press 3 To Check Balance" << endl;
+                    cout << "Press 4 For Account Details" << endl;
+                    cout << "Press 0 To Exit" << endl;
+                    cout << "Enter your Choice :- ";
+                    cin >> choice_1;
+                    cout << endl;
+
+                    switch (choice_1)
+                    {
+                    case 0:
+                        cout << endl << "---- Exited Scucessfully ----";
+                        break;
+                    case 1:
+                        obj.deposit();
+                        break;
+                    
+                    case 2:
+                        obj.withdraw();
+                        break;
+
+                    case 3:
+                        cout << "Current Balance :- " << obj.getBalance() << endl;
+                        break;
+                    
+                    case 4:
+                        obj.displayAccountInfo();
+                        break;
+
+                    default:
+                        cout << "Sorry Wrong Choice." << endl;
+                        break;
+                    }
+                } 
+                while 
+                (choice_1 != 0);
+                break;
+            case 2:
+                Myobj_2.calculateIntrest();
+                break;   
+
+            case 3:
+                Myobj_3.DraftCheaker();
+                break;   
+
+            case 4:
+                Myobj_4.calculateIntrest();
+                break;   
+
+            default:
+                cout << "Sorry Wrong Choice." << endl;
+                break;
+        }
+    } while (main_choice !=0);
+    
     return 0;
 }
